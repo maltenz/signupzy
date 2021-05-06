ccaa71adf958c2dc1a79a1ded721b8761b075176
Malte <mbo@demodern.de>
2021-05-07 10:47:37 +1200

    Remove form controller

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,35 +1,90 @@
-import { FC } from 'react';
-import { createUseStyles } from 'react-jss';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import { createUseStyles } from 'react-jss';
 
-import InputField from '../components/InputField';
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const classes = useStyles();
+
+  const onSubmit = (data: any) => {
+    alert(JSON.stringify(data));
+  };
+
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={classes.inputField}>
+        <label>
+          Username
+          <div className={classes.inputWrapper}>
+            <input type="text" {...register('username', { required: true, maxLength: 80 })} />
+          </div>
+        </label>
+      </div>
+      <div className={classes.inputField}>
+        <label>
+          Email
+          <div className={classes.inputWrapper}>
+            <input
+              type="email"
+              {...register('email', {
+                required: true,
+                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
+            />
+          </div>
+        </label>
+      </div>
+      <div className={classes.inputField}>
+        <label>
+          Password
+          <div className={classes.inputWrapper}>
+            <input
+              type="password"
+              {...register('password', {
+                required: true,
+                pattern: /(?=.*[0-9])/,
+              })}
+            />
+          </div>
+        </label>
+      </div>
+      <div className={classes.inputField}>
+        <label>
+          Confirm Password
+          <div className={classes.inputWrapper}>
+            <input
+              type="password"
+              {...register('confirmPassword', {
+                required: true,
+                validate: (value) => value === watch('password'),
+              })}
+            />
+          </div>
+        </label>
+      </div>
+      <input type="submit" />
+    </form>
+  );
+};
 
 const useStyles = createUseStyles({
-  formContainer: {
-    display: 'flex',
-    height: '100vh',
-    width: '100%',
-    margin: '1em',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   form: {
-    padding: '2em',
-    border: '1px #cac3c3 solid',
-    borderRadius: 8,
+    width: 380,
   },
-  formHeader: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '2em',
-  },
-
   inputField: {
     margin: '0.5em 0',
   },
-
   inputWrapper: {
-    margin: '0.4em 0',
+    margin: '1em 0',
     boxSizing: 'border-box',
     '& input': {
       font: 'inherit',
@@ -38,74 +93,9 @@ const useStyles = createUseStyles({
       padding: '0.8em',
       borderRadius: 4,
       border: '0.5px grey solid',
+      width: '100%',
     },
   },
 });
 
-interface IFormInput {
-  userName: String;
-  email: String;
-  password: String;
-  confirmPassword: String;
-}
-
-const SignUp: FC = () => {
-  const classes = useStyles();
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFormInput>();
-  const onSubmit = (data: IFormInput) => console.log(data);
-  console.log({ errors });
-  return (
-    <div className={classes.formContainer}>
-      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={classes.formHeader} role="heading" aria-level={1}>
-          Signupzy
-        </div>
-
-        <p>Welcome, please signup</p>
-
-        <div>
-          <div>Error</div>
-        </div>
-
-        <InputField
-          label="Username"
-          control={control}
-          name="username"
-          defaultValue={''}
-        />
-        <InputField
-          label="Email"
-          control={control}
-          name="email"
-          validate={(value) => {
-            const re = /^\S+@\S+\.\S+$/;
-            return 'Invalid Email Address';
-            // return re.test(value as string) ? '' : 'Invalid Email Address';
-          }}
-          defaultValue={''}
-        />
-        <InputField
-          label="Password"
-          control={control}
-          name="password"
-          defaultValue={''}
-        />
-        <InputField
-          label="Confirm Password"
-          control={control}
-          name="confirmPassword"
-          defaultValue={''}
-        />
-
-        <button type="submit">submit</button>
-      </form>
-    </div>
-  );
-};
-
-export default SignUp;
+export default Form;
